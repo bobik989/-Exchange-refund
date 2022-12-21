e2de9e7439ffe6d7db62a2bb337b72957c1d2d4f
bobik989 <107407035+bobik989@users.noreply.github.com>
2022-12-21 15:52:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+product_money = int(input('Сколько стоит сумма всех товаров: '))
+user_money = int(input('Сколько вы даете денег продавцу: '))
+money = [1, 2, 5, 10]
+fin_dif = []
+
+difa = user_money - product_money
+dif = difa
+while dif > 0:
+
+    if dif > money[3]:
+        dif -= 10
+        fin_dif.append(money[3])
+
+    elif dif > money[2]:
+        dif -= 5
+        fin_dif.append(money[2])
+
+    elif dif > money[1]:
+        dif -= 2
+        fin_dif.append(money[1])
+
+    elif dif >= money[0]:
+        dif -= 1
+        fin_dif.append(money[0])
+        break
+
+print(f'Ваша сдача должна составлять {dif} \n по мелким начилным это: {fin_dif}')
